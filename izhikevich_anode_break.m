9699a39f839b5016d9bfee3e347432955077f655
rakaar <rka87338@gmail.com>
2021-12-19 23:16:08 +0530

    not crossing -60

--- izhikevich_anode_break.m
+++ izhikevich_anode_break.m
@@ -7,7 +7,7 @@ function project
     b = -1;
     v_0 = -65; % source - http://www.columbia.edu/cu/appliedneuroshp/Spring2018/Spring18SHPAppliedNeuroLec5.pdf
     u_0 = b * v_0;
-    [t r] = ode15s(@izhikevich_model, [0 50], [v_0 u_0]);
+    [t r] = ode15s(@izhikevich_model, [0 300], [v_0 u_0]);
 
     figure(1)
         plot(t, r(:,1));
@@ -20,8 +20,8 @@ function result = izhikevich_model(t,r)
     c1 = 0.04; c2 = 5; c3=140; c4=1; c5=1;
     a=0.03;b=0.25;c=-60;d=4;
 
-    if t < 20
-        i = -10;
+    if t < 20 & t > 10
+        i = -1.6;
     else
         i = 0;
     end
