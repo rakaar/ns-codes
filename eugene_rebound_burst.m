740c3fa0007bb7903bb7ccbb0d5d3b4dd31d2734
rakaar <rka87338@gmail.com>
2021-12-23 19:25:07 +0530

    comit before writing loop

---
+++ eugene_rebound_burst.m
@@ -0,0 +1,29 @@
+% for i=-5:1:5
+    a=0.5; b=0.50; c=-30;  d=8;
+    V=-64;  u=b*V;
+    VV=[];  uu=[];
+    tau = 0.2;  tspan = 0:tau:200;
+    T1=20;
+    for t=tspan
+        if (t>T1) & (t < T1+5) 
+            I=-15;
+        else
+            I=0;
+        end;
+        V = V + tau*(0.04*V^2+5*V+140-u+I);
+        u = u + tau*a*(b*V-u);
+        if V > 30
+            VV(end+1)=30;
+            V = c;
+            u = u + d;
+        else
+            VV(end+1)=V;
+        end;
+        uu(end+1)=u;
+    end;
+    figure(floor(100+1))
+        plot(tspan,VV,[0 T1 T1 (T1+5) (T1+5) max(tspan)],-85+[0 0 -5 -5 0 0]);
+        axis([0 max(tspan) -90 30])
+        title([' value ', num2str(i)]);
+    grid
+% end
