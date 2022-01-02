8c2702d3740b877266fc51fe0f81d6d1c9fe13c3
rakaar <rka87338@gmail.com>
2022-01-02 10:49:56 +0530

    one more ifb

--- copy_ifb_scratch.m
+++ copy_ifb_scratch.m
@@ -7,15 +7,15 @@ i1 = 0; % we are not bothered about sinosidal curents
 f=1;
 
 g_leak = 0.035;
-v_theta = -45; % try for -46, -47, -48
+v_theta = -35; % try for -46, -47, -48
 v_reset = -50 ;
-v_leak = -25;
-v_h = -70;
-v_t = 250;
+v_leak = -40;
+v_h = -45;
+v_t = 120;
 
-g_t = 0.01;
+g_t = 0.07;
 
-tau_h_plus = 90;
+tau_h_plus = 0.05;
 tau_h_minus = 100;
 
 v_arr = [];
@@ -24,12 +24,12 @@ h_arr = [];
 dt = 0.001; t_inital=0; t_final=200;
 T1=20;
 
-v_0 = -70; h_0 = 0.2;
+v_0 = v_leak; h_0 = 0.05;
 v = v_0; h  = h_0;
 
 for t=t_inital:dt:t_final
-    if (t>T1) & (t < T1+5) 
-        i0=-2;
+    if (t>T1) & (t < T1+20) 
+        i0=-0.3;
     else
         i0=0;
     end;
@@ -58,14 +58,14 @@ for t=t_inital:dt:t_final
 end
 
 figure(12)
-    plot(t_inital:dt:t_final, v_arr, [0 T1 T1 (T1+5) (T1+5) t_final],-55+[0 0 -5 -5 0 0]);
+    plot(t_inital:dt:t_final, v_arr, [0 T1 T1 (T1+20) (T1+20) t_final],-55+[0 0 -5 -5 0 0]);
     axis([0 t_final -80 -30]);
 grid
 
 figure(13)
     hold on
     scatter(t_inital:dt:t_final,h_arr);
-    plot([0 T1 T1 (T1+5) (T1+5) t_final], [0 0 -0.2 -0.2 0 0]);
+    plot([0 T1 T1 (T1+5) (T1+5) t_final], [0 0 -0.02 -0.02 0 0]);
     hold off
 grid
 
