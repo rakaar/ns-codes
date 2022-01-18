d06ceae5214be74426256b0596bf9a7077fe2d93
rakaar <rka87338@gmail.com>
2022-01-18 15:28:51 +0530

    round

--- izhikevich_new.m
+++ izhikevich_new.m
@@ -1,9 +1,13 @@
 a=0.03; b=0.25; c=-52;  d=0;
 V=-64;  u=b*V;
-VV=[];  uu=[];
-tau = 0.2;  tspan = 0:tau:200;
+T_end = 200;
+tau = 0.2;  tspan = 0:tau:T_end;
 T1=20;
 current_span = 100;
+
+VV=zeros(1, length(tspan));  
+uu=zeros(1, length(tspan));
+
 
 for t=tspan
     if (t>T1) & (t < T1+current_span) 
@@ -14,13 +18,13 @@ for t=tspan
     V = V + tau*(0.04*V^2+5*V+140-u+I);
     u = u + tau*a*(b*V-u);
     if V > 30
-        VV(end+1)=30;
+        VV(1, round(t/tau + 1))=30;
         V = c;
         u = u + d;
     else
-        VV(end+1)=V;
+        VV(1, round(t/tau + 1))=V;
     end;
-    uu(end+1)=u;
+    uu(1, round(t/tau + 1))=u;
 end;
 plot(tspan,VV,[0 T1 T1 (T1+current_span) (T1+current_span) max(tspan)],-85+[0 0 -5 -5 0 0]);
 axis([0 max(tspan) -90 30])
