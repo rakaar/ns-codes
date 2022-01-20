b3f7960745eedf3998049908f1a14e68ad571d49
rakaar <rka87338@gmail.com>
2022-01-20 22:11:14 +0530

    stuck

--- ssa_rnn/column.m
+++ ssa_rnn/column.m
@@ -1,4 +1,12 @@
-function column()
-    [voltage_arr feedback_var_arr] = neuron(0.03, 0.25, -52, 0,-20, 2);
+classdef column
+    properties
+        N_excitatory = 100;
+        N_inhibitory = 25;
+        
+    end
     
+    methods
+        function obj = column(weights)
+        end
+    end
 end
--- ssa_rnn/run_single_neuron.m
+++ ssa_rnn/run_single_neuron.m
@@ -1,5 +1,10 @@
-n1 = neuron(0.03, 0.25, -52, 0);
+% time 
+T_end = 700;
+dt = 0.2; 
+tspan = 0:dt:T_end;
 
+n1 = neuron(0.03, 0.25, -52, 0);
+ 
 negative_current=-20;
 T1=20; 
 negative_curent_span=200;
@@ -11,5 +16,5 @@ positive_current_span = 30;
 n1 = n1.fire(negative_current, T1, negative_curent_span, positive_current, T2, positive_current_span);
 
 figure(1)
-    plot(n1.voltage)
+    plot(tspan, n1.voltage)
 grid
