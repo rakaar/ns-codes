3005cefd582f69cc087fbad53b2a0dbe9d38f22e
rakaar <rka87338@gmail.com>
2022-01-22 11:17:59 +0530

    wrong way of ading xe

--- procedural_network/voltage_to_spikes.m
+++ procedural_network/voltage_to_spikes.m
@@ -2,8 +2,9 @@ function spikes = voltage_to_spikes(voltage)
     spikes = zeros(1, length(voltage));
     
     for i=1:length(voltage)
-        if voltage(1,i) == 1
+        if voltage(1,i) == 30
             spikes(1, i) = 1;
         end
     end
+
 end
