43cfe278c61736f9992619dea783cdd274f5a032
rakaar <rka87338@gmail.com>
2022-02-13 16:34:49 +0530

    comit before epsc g t vector in sim loop

---
+++ modelling2/get_g_t_vector.m
@@ -0,0 +1,6 @@
+function g_t_vector = get_g_t(spike_train, period)
+    tau_syn = 10;
+	kernel_kt = [0 exp(-[0:length(spike_train)-1])./tau_syn];
+    g_t_vector = conv(spike_train, kernel_kt);
+    g_t_vector = g_t_vector(1, 1:period);
+end
