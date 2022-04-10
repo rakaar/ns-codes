2d5bed31103e9a232073bab69be48cf2551dffa5
rakaar <rka87338@gmail.com>
2022-04-10 13:50:37 +0530

    very low spont, clip test also almost pass

--- modelling2_gif/calculate_new_state_exp.m
+++ modelling2_gif/calculate_new_state_exp.m
@@ -3,9 +3,9 @@ function [v, i1, i2, theta, is_spike] = calculate_new_state_exp(v_old, i1_old, i
     
 %      background element causing spike
 % some how adding this background element results prevents disinhibtion
-%      if rand <= 0.005
-%         is_spike = 1;
-%      end
+     if rand <= 0.001
+        is_spike = 1;
+     end
 
     k1=0.2;k2=0.02;b=0.01;R1=0.0;R2=1.0;
     El=-70.0;Vr=-70.0;Thetar=-40.0;G=0.05;C=1.0;ThetaInf=-50.0;
