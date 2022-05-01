ede174d09c79e48a2963eaf9a2ba13693d62fcc5
rakaar <rka87338@gmail.com>
2022-05-01 10:39:32 +0530

    disinhi in some, in not some, but +ve after stim

--- modelling2_gif/a1.m
+++ modelling2_gif/a1.m
@@ -4,7 +4,7 @@ close all;
 n_iters = 5;
 
 % basic variables;
-n_columns = 1
+n_columns = 1;
 n_excitatory = 20; 
 n_inhibitory = 5; 
 n_total_neurons = n_excitatory + n_inhibitory;
@@ -67,7 +67,7 @@ lamda = zeros(1, length(tspan));
 % 100ms - 3-4 spikes, 200ms - 18-20 spikes, 300 - rest - 3-4 spikes
 % WARNING: FOR NOW THIS STIMULS IS HARD CODED, need to adjust acc to
 % t_simulate
-lamda_s = 250; lamda_i = 1;
+lamda_s = 250; lamda_i = 0;
 for i=1:500
     lamda(1,i) = lamda_i;
 end
