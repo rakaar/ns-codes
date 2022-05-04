0f1325224db8e2904e76ed53ffdb6679e2ef2a26
rakaar <rka87338@gmail.com>
2022-05-04 21:59:36 +0530

    plotting almost everything for a iter

--- modelling2_gif/single_col_plot_iter_wise.m
+++ modelling2_gif/single_col_plot_iter_wise.m
@@ -14,7 +14,7 @@ hold on
         end
         
 
-        plot(mean_spike_rate_of_random_neuron*100);
+        plot(mean_spike_rate_of_random_neuron*100,'LineStyle','--');
 
         % adjusting from length(tspan)-1 to length(tspan)
         epsc_all_iters_for_random_neuron = squeeze(total_input_epsc(:, col, random_neuron, :));
@@ -30,18 +30,41 @@ hold on
         end
         
         epsc_to_random_neuron_binned = spikes_to_spike_rate_neat(random_epsc_iters_avg, physical_time_in_ms, dt, spike_rate_dt);
-        plot(epsc_to_random_neuron_binned)
+        plot(epsc_to_random_neuron_binned,'m')
         threshold = squeeze(theta_tensor(iter_to_see, col, random_neuron, :));
         threshold_binned = spikes_to_spike_rate_neat(threshold, physical_time_in_ms, dt, spike_rate_dt);
-        plot(threshold_binned);
+        plot(threshold_binned, 'c');
 
         v = squeeze(voltages(iter_to_see, col, random_neuron, :));
         v_binned = spikes_to_spike_rate_neat(v, physical_time_in_ms, dt, spike_rate_dt);
-        plot(v_binned);
+        plot(v_binned,'r');
+
+
+        %  thalamic current
+        thalamic_curr_to_neuron = thalamic_epsc_tensor(iter_to_see, col, random_neuron, :);
+        thalamic_curr_to_neuron = squeeze(thalamic_curr_to_neuron);
+        thalamic_curr_to_neuron = [0; thalamic_curr_to_neuron];
+        plot(thalamic_curr_to_neuron,'g');
+    
+        %  exc epsc
+        exc_epsc = recurrence_exc_self_column_epsc_tensor(iter_to_see, col, random_neuron, :) + recurrence_exc_neighbour_column_epsc_tensor(iter_to_see, col, random_neuron, :);
+        exc_epsc = squeeze(exc_epsc);
+        exc_epsc = [0; exc_epsc];
+        plot(exc_epsc,'b');
+
+        % plot inh epsc
+        inh_epsc = recurrence_inh_self_column_epsc_tensor(iter_to_see, col, random_neuron, :) + recurrence_inh_neighbour_column_epsc_tensor(iter_to_see, col, random_neuron, :);
+        inh_epsc = squeeze(inh_epsc);
+        inh_epsc = [0; inh_epsc];
+        plot(inh_epsc,'k');
+
+        % plot recurrence epsc
+        rec_epsc = exc_epsc + inh_epsc;
+        plot(rec_epsc,'y');
 
     hold off
         title('random neuron epsc and psth')
-        legend('psth', 'epsc', 'threshold', 'voltage')
+        legend('psth', 'epsc', 'threshold', 'voltage', 'thalamic current', 'exc epsc', 'inh epsc', 'rec epsc')
 grid
 pause
 clf
