4ba54da2b7b13e95cf946ea95a272cf195eb7675
rakaar <rka87338@gmail.com>
2022-05-06 11:57:32 +0530

    single col seems good disinhi

--- modelling2_gif/a1.m
+++ modelling2_gif/a1.m
@@ -23,7 +23,7 @@ spike_rate_length = (length(tspan)-1)/(spike_rate_dt/dt);
 
 % connection strength
 weight_reducing_l4 = 1; % for now all weights reduced by factor of 0.2
-increase_inhibitory_factor = 120;
+increase_inhibitory_factor = 150;
 weight_exc_factor = 15;
 J_ee_0 = 6*weight_reducing_l4*weight_exc_factor; 
 J_ie_0 = 0.5*weight_reducing_l4*weight_exc_factor;
