fcb5753aa3700864d1291b09e9e3e6c28a56f0fd
rakaar <rka87338@gmail.com>
2022-05-09 16:08:14 +0530

    comit beforing working on spont

--- modelling2_gif/a1.m
+++ modelling2_gif/a1.m
@@ -1,7 +1,7 @@
 clear all;
 close all;
 
-n_iters = 20;
+n_iters = 2;
 
 % basic variables;
 n_columns = 1;
@@ -456,6 +456,36 @@ figure
         plot(mean_spike_rate_for_neurons)
     hold off
     title('total input and psth')
+    legend('total input epsc', 'psth l4')
+grid
+
+
+% 5 bins - psth and epsc
+spike_rate_dt_5 = 5*dt;
+spike_rate_length_5 = (length(tspan)-1)/(spike_rate_dt_5/dt);
+spike_rates_5 = zeros(n_iters, n_columns, n_total_neurons, spike_rate_length_5);
+
+for i=1:n_iters
+    for n=1:n_total_neurons
+        spikes1 = spikes(i, 1, n, :);
+        spikes1_reshaped = reshape(spikes1, 1,length(tspan));
+        spike_rate1 = spikes_to_spike_rate_neat(spikes1_reshaped, physical_time_in_ms, dt, spike_rate_dt_5);
+        spikes_rate1 = reshape(spike_rate1, 1,1,1,spike_rate_length_5);
+        spike_rates_5(i,1,n,:) = spikes_rate1; 
+    end
+end
+
+figure
+     [mean_spike_rate_for_iters, mean_spike_rate_for_neurons] = get_mean(spike_rates_5, n_iters, n_total_neurons, spike_rate_length_5,1);
+        n_bins = spike_rate_dt_5/dt;
+    hold on
+        mean_input_epsc_extended = zeros(1, length(tspan));
+        mean_input_epsc_extended(1, 2:length(tspan)) = mean_input_epsc_all_for_neurons;
+        mean_input_epsc_binned = spikes_to_spike_rate_neat(mean_input_epsc_extended, 1, dt, spike_rate_dt_5);
+        plot(mean_input_epsc_binned*(n_bins*physical_time_in_ms*0.001))
+        plot(mean_spike_rate_for_neurons)
+    hold off
+    title('5 bins total input and psth')
     legend('total input epsc', 'psth l4')
 grid
 
