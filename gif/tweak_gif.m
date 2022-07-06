a03cd5316299e05e9a01b4d312a497ec58f3c5c8
rakaar <rka87338@gmail.com>
2022-07-06 16:53:42 +0530

    single neuron almost sorted

--- gif/tweak_gif.m
+++ gif/tweak_gif.m
@@ -19,10 +19,9 @@ close all;clear all;
 k1=0.2;k2=0.02;b=0.01;R1=0.0;R2=1.0;
 El=-70.0;Vr=-70.0;Thetar=-60.0;G=0.05;C=1.0;ThetaInf=-50.0;
 
-% k2=0.01;
 
 % a = 0.005; A1 = 10; A2 = -0.6;
-a = 0.010; A1 = 10; A2 = -0.15;
+a = 0.020; A1 = 5; A2 = -0.1;
 
 t_simulate = 2000;
 dt = 1; % may be 1ms
@@ -34,8 +33,8 @@ v = zeros(1, length(tspan));
 theta = zeros(1, length(tspan));
 spikes = zeros(1, length(tspan));
 iext = zeros(1, length(tspan));
-iext(950:1000) = -15;
-% iext(950:1000) = 300;
+iext(950:1000) = -20;
+% iext(950:1000) = 20;
 
 % params for phasic burst and rebound burst
 % a = 0.009; A1 = 15; A2 = -0.6; 
@@ -77,8 +76,9 @@ for t=2:length(tspan)
 %         disp('------------------------')
 %     end
     theta(t) = theta(t-1) + dt*( a*(v(t-1) - El) - b*(theta(t-1) - ThetaInf)  );
-    if v(t) < -140
-        v(t)= -140;
+    v_limit = -100;
+    if v(t) < v_limit
+        v(t)= v_limit;
     end
     if v(t) > theta(t)
         i1(t) = R1*i1(t) + A1;
@@ -108,3 +108,90 @@ a = x(1);b = length(x);c=x(end);
 disp(a)
 disp(c)
 disp(b)
+
+
+% %% for negative current
+% t_simulate = 2000;
+% dt = 1; % may be 1ms
+% tspan = 0:dt:t_simulate;
+% 
+% i1 = zeros(1, length(tspan));
+% i2 = zeros(1, length(tspan));
+% v = zeros(1, length(tspan));
+% theta = zeros(1, length(tspan));
+% spikes = zeros(1, length(tspan));
+% iext = zeros(1, length(tspan));
+% iext(950:1000) = -15;
+% % iext(950:1000) = 300;
+% 
+% % params for phasic burst and rebound burst
+% % a = 0.009; A1 = 15; A2 = -0.6; 
+% 
+% % params fo tonic spike through spike for +ve current
+% % a = 0; A1 = 0; A2 = 0; 
+% 
+% % 
+% % a = 0.005; A1 = 10; A2 = -0.6;
+% % 
+% % % adjusting for realistic voltage values
+% % C = 3;
+% % b = 0.001;
+% % a = 0.01;
+% 
+% % IC=(0.01, 0.001, -70.0, -50.0)
+% % initial conditions
+% i1(1) = 0.01;
+% i2(1) = 0.001;
+% v(1) =-70.0 ;
+% theta(1) = -50.0;
+% 
+% % temporrary
+% x = [];
+% for t=2:length(tspan)
+%     i1(t) = i1(t-1) + dt*(-k1*i1(t-1));
+% %     if t > 940 && t < 960
+% %         disp("-----------------")
+% %         disp(dt*(-k1*i1(t-1)))
+% %     end
+%     i2(t) = i2(t-1) + dt*(-k2*i2(t-1));
+%     v(t) = v(t-1) + dt*(1/C)*(iext(t-1)+ i1(t-1) + i2(t-1) - G*(v(t-1) - El));
+% %     if t > 1000 && t < 1050
+% %         disp(dt*(1/C)*(iext(t-1)+ i1(t-1) + i2(t-1) - G*(v(t-1) - El)))
+% %         disp(i1(t-1))
+% %         disp(i2(t-1))
+% %         disp(i2(t-1))
+% %         disp(-G*(v(t-1) - El))
+% %         disp('------------------------')
+% %     end
+%     theta(t) = theta(t-1) + dt*( a*(v(t-1) - El) - b*(theta(t-1) - ThetaInf)  );
+%     if v(t) < -140
+%         v(t)= -140;
+%     end
+%     if v(t) > theta(t)
+%         i1(t) = R1*i1(t) + A1;
+%         i2(t) = R2*i2(t) + A2;
+%         v(t) = Vr;
+%         theta(t) = max(Thetar, theta(t));
+% 
+%         spikes(t) = 1;
+%        x = [x, t];
+%     end
+% end
+% 
+% figure
+%     hold on
+%         plot(v)
+%         plot(theta)
+%         plot(spikes*100)
+%         plot(i1)
+%         plot(i2)
+%         plot(iext)
+%         legend('v', 'theta', 'spikes','i1','i2', 'iext')
+%         title('voltage and threshold and spikes')
+%     hold off
+% grid
+% 
+% a = x(1);b = length(x);c=x(end);
+% disp(a)
+% disp(c)
+% disp(b)
