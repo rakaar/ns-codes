b2d2fc47a0170d8c8cef306d39dca17d4a191a3b
rakaar <rka87338@gmail.com>
2022-08-04 13:15:54 +0530

    inc 2.5% to run on meghamala

--- modelling2_gif/all_analysis_batches.m
+++ modelling2_gif/all_analysis_batches.m
@@ -1,7 +1,7 @@
 %% basic vars
 batches = 100;
-data_path = "D:\both-pv-som-w-1.05-som-off";
-images_path = "D:\both-pv-som-w-1.05-som-off-analysis\";
+data_path = "D:\both-pv-som-0.025-decrease-som-on";
+images_path = "D:\both-pv-som-0.025-decrease-som-on-analysis\";
 n_columns = 5;
 n_excitatory=20; n_pv = 3; n_som  = 2;
 n_neurons = n_excitatory + n_pv + n_som;
